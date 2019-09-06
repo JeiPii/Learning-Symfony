b340d58672a4c09e98d2c2189431198e4e5d67c8
JeiPii <jp.wijn@gmail.com>
2019-09-06 15:11:01 +0200

    Heart mechanic added

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,11 +15,11 @@ class ArticleController extends AbstractController
      */
     public function homepage()
     {
-        return new Response('First page');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/{slug}", name="news")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
@@ -34,5 +35,15 @@ class ArticleController extends AbstractController
             'title' => ucwords(str_replace('-', ' ', $slug)),
             'comments' => $comments,
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart")
+     */
+
+    public function  toggleArticleHeart($slug){
+        // TODO -actually heart/un heart the article!
+
+        return new JsonResponse(['hearts' => rand(5,100)]);
     }
 }
